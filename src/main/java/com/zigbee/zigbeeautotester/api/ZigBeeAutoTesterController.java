71c8491ee906c14d3c966edb3e4db6c7ad49a87f
Maksim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2023-02-14 20:17:14 +0300

    Implementing COM Port Reader

--- src/main/java/com/zigbee/zigbeeautotester/api/ZigBeeAutoTesterController.java
+++ src/main/java/com/zigbee/zigbeeautotester/api/ZigBeeAutoTesterController.java
@@ -2,9 +2,9 @@ package com.zigbee.zigbeeautotester.api;
 
 import com.fazecast.jSerialComm.SerialPort;
 import com.zigbee.zigbeeautotester.model.ZigbeeTestPreset;
+import com.zigbee.zigbeeautotester.service.ZigBeeAutoTesterService;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +14,11 @@ import java.util.List;
 public class ZigBeeAutoTesterController {
 
     private final ZigbeeTestPreset testPreset;
+    private final ZigBeeAutoTesterService zigBeeAutoTesterService;
 
-    public ZigBeeAutoTesterController(ZigbeeTestPreset testPreset) {
+    public ZigBeeAutoTesterController(ZigbeeTestPreset testPreset, ZigBeeAutoTesterService zigBeeAutoTesterService) {
         this.testPreset = testPreset;
+        this.zigBeeAutoTesterService = zigBeeAutoTesterService;
     }
 
     @GetMapping("/api/v1.0/zat/greetings")
@@ -48,6 +50,34 @@ public class ZigBeeAutoTesterController {
 
     }
 
+    @PostMapping("/api/v1.0/zat/presets/port")
+    public boolean setZigBeeHubLogPort(@RequestParam("port_name") String portName) {
+
+        List<String> result = new ArrayList<>();
+
+        SerialPort[] ports = SerialPort.getCommPorts();
+
+        for (SerialPort port: ports) {
+            result.add(port.getSystemPortName());
+        }
+
+        log.info("Port Name: {}",  portName);
+
+        if (result.contains(portName)) {
+            testPreset.setExpectedSerialPort(portName);
+            return true;
+        }
+
+        return false;
+    }
+
+    @PostMapping("/api/v1.0/zat/start")
+    public ZigbeeTestPreset startTests() {
+
+        zigBeeAutoTesterService.StartTest();
+        return testPreset;
+
+    }
 
 
 }
--- src/main/java/com/zigbee/zigbeeautotester/service/ZigBeeAutoTesterService.java
+++ src/main/java/com/zigbee/zigbeeautotester/service/ZigBeeAutoTesterService.java
@@ -1,11 +1,78 @@
 package com.zigbee.zigbeeautotester.service;
 
+import com.fazecast.jSerialComm.SerialPort;
 import com.zigbee.zigbeeautotester.model.ZigbeeTestPreset;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+
 @Service
+@Log4j2
 public class ZigBeeAutoTesterService {
 
+    private final ZigbeeTestPreset testPreset;
 
+    public ZigBeeAutoTesterService(ZigbeeTestPreset testPreset) {
+        this.testPreset = testPreset;
+    }
+
+    public void StartTest() {
+
+        var future = CompletableFuture.supplyAsync(()-> {
+            try {
+                return ZigBeeHubLogsReader();
+            } catch (IOException e) {
+                return 0;
+            }
+        }).thenApply(result -> {log.info("Port was closed"); return 0;} );
+
+    }
+
+
+    private double ZigBeeHubLogsReader() throws IOException {
+
+        SerialPort comPort = null;
+        for (var port : SerialPort.getCommPorts())
+        {
+            log.info("Current Port: {} Preset Port: {}", port.getSystemPortName(), testPreset.getExpectedSerialPort());
+            if (port.getSystemPortName().equals(testPreset.getExpectedSerialPort())) {
+                comPort = port;
+                break;
+            }
+        }
+
+        if (comPort == null)
+            return 0;
+
+        log.info("Trying to open");
+
+        comPort.setComPortParameters(115200, 8, 1, 0);
+
+        comPort.openPort();
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+        comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        InputStream in = comPort.getInputStream();
+        double readBytes = 0;
+        try
+        {
+            String current = "";
+            while (true) {
+                var ch = (char)in.read();
+                current += ch;
+                if (ch == '\n') {
+                    log.info(current);
+                    current = "";
+                }
+                readBytes++;
+            }
+
+        } catch (Exception e) { e.printStackTrace(); }
+        in.close();
+        comPort.closePort();
+        return readBytes;
+    }
 
 }
